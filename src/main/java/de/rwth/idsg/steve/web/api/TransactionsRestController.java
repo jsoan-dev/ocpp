ab173616eb1c1ee36ae337a418de0c3bfcbb068c
Jashon Osala <64925863+osala-eng@users.noreply.github.com>
2025-09-28 18:32:11 +0300

    Fix missing Spring MVC imports

--- src/main/java/de/rwth/idsg/steve/web/api/TransactionsRestController.java
+++ src/main/java/de/rwth/idsg/steve/web/api/TransactionsRestController.java
@@ -39,6 +39,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
